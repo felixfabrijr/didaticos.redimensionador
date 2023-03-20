3f636420733c57761254376f4a02f8be06182294
felixfabri <felix.fabrijr@gmail.com>
2023-03-20 11:40:34 +0000

    Add project files.

---
+++ didaticos.redimensionador/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace didaticos.redimensionador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            | Console.WriteLine("Iniciando redimensionador");
+
+
+
+
+        }
+    }
+}
